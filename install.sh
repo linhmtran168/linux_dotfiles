ab185267fdecb2a508c3521bfae42e07ea7b5ffe
Linh Tran <linh.mtran168@live.com>
2022-01-24 17:55:01 +0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+# Create ssh key => do manually => upload to github
+# ssh-keygen -t rsa -b 4096 -C "email"
+# Clone dotfiles directory
+cd ~
+git clone git@github.com:linhmtran168/linux_dotfiles.git
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install brew package
+brew install python fish python vim tmux cmake dust diff-so-fancy bat sd hyperfine exa fd fzf xh z \
+    ghq rbenv golang helm hadolint htop minikube ripgrep sd starship tokei \
+    tealdeer cheat curlie git-delta duf procs broots terraform kubernetes-cli direnv
+brew install clementtsang/bottom/bottom
+
+# Fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+# Change default shell to fish
+# Must run sudo vim /etc/shells to add fish to list of shell
+chsh -s /usr/bin/fish
+## OMF
+curl -L https://get.oh-my.fish | fish
+omf update
+omf install python rustup z fzf
+
+# Symlink config file for git and fish
+ln -sf ~/linux_dotfiles/.gitignore ~/.gitignore
+cp ~/linux_dotfiles/.gitconfig ~/.gitconfig # must manually change email in git config
+ln -sf ~/linux_dotfiles/fish_conf/init.fish ~/.config/omf/init.fish
+ln -sf ~/linux_dotfiles/fish_conf/key_bindings.fish ~/.config/omf/key_bindings.fish
+ln -sf ~/linux_dotfiles/starship.toml ~/.config/starship.toml
+
+# Vim
+ln -sf ~/linux_dotfiles/.vimrc ~
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/undodir
+
+# Base 16
+ghq get git@github.com:martinlindhe/base16-iterm2.git # Manually change theme using iterm config
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell # Use base16-xxx shell command to change theme
+
+# Powerline
+pip3 install powerline-status
+pip3 install psutil
+ghq get git@github.com:powerline/powerline.git
+
+# Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sf ~/mac_dotfiles/.tmux.conf ~/.tmux.conf
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+nvm use --lts
+nvm alias default node
+npm -g install eslint
+
+# Ruby
+rbenv install 2.7.3
+rbenv global 2.7.3
+gem install pry awesome_print bundler rubocop
+
+# Rust
+curl https://sh.rustup.rs -sSf | sh
+
+# Docker
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+newgrp docker
+
